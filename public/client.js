d8d7480005761dadb43f2ce396bd11d3a853d8f4
Glitch (hello-express) <none>
2020-04-08 11:04:18 +0000

    🐇👲 Checkpoint ./public/client.js:546259/187

--- public/client.js
+++ public/client.js
@@ -1,3 +1,14 @@
-const session = OT.initSession(apiKey, sessionId);
+function initializeSession(apiKey, sessionId, token) {
+  const session = OT.initSession(apiKey, sessionId);
+  
+  const publisher = OT.initPublisher(
+    "publisher",
+    {
+      insertMode: "append",
+      width: "100%",
+      height: "100%"
+    },
+    handleCallback
+  )
 
-const publisher = OT.initPublisher("publisher")
+}
