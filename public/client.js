ae42ae453bf1535f250991633cd4ac8d2bdf5b50
Glitch (hello-express) <none>
2020-04-08 11:15:20 +0000

    🚆☔️ Checkpoint ./public/client.js:546259/868 ./public/style.css:546259/713

--- public/client.js
+++ public/client.js
@@ -1,3 +1,15 @@
+fetch(location.pathname, { method: "POST" })
+  .then(res => {
+    return res.json();
+  })
+  .then(res => {
+    const apiKey = res.apiKey;
+    const sessionId = res.sessionId;
+    const token = res.token;
+    initializeSession(apiKey, sessionId, token);
+  })
+  .catch(handleCallback)
+
 function initializeSession(apiKey, sessionId, token) {
   const session = OT.initSession(apiKey, sessionId);
   
@@ -9,6 +21,34 @@ function initializeSession(apiKey, sessionId, token) {
       height: "100%"
     },
     handleCallback
-  )
+  );
+  
+  session.connect(token, error => {
+    if(error) {
+      handleCallback(error);
+    } else {
+      session.publish(publisher, handleCallback)
+    }
+  });
+  
+  session.on("streamCreated", event => {
+    session.subscribe(
+      event.stream, 
+      "subscriber",
+      {
+        insertMode: "append",
+        width: "100%",
+        height: "100%"
+      },
+      handleCallback
+    )
+  })
+}
 
+function handleCallback(error) {
+  if (error) {
+    console.log("error: " + error.message);
+  } else {
+    console.log("callback success");
+  }
 }
